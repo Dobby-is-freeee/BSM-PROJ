e4065969f9141baf8339d7f325f3e6d95e477fa1
Kabsung Lee <54669100+Closer94@users.noreply.github.com>
2022-04-08 17:59:43 +0900

    feat: 선수 회원 가입, 선수 기록 등록 기능 구현

--- src/main/java/com/example/bsm/controller/HomeController.java
+++ src/main/java/com/example/bsm/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.bsm.controller;
 
+import com.example.bsm.dto.Member;
+import com.example.bsm.dto.MemberForm;
+import com.example.bsm.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Hyunsik Lee on 2022-03-31. Blog : https://hs95blue.github.io/ Github :
@@ -10,8 +15,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  @GetMapping("/")
-  public String Home(){
-    return "home";
-  }
+    @GetMapping("/")
+    public String Home() {
+        return "home";
+    }
+
+
 }
