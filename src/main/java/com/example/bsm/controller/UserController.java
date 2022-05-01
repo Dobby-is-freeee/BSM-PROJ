a739a1a6db83ce8dfadf6051131ed1d0ef33d305
Kabsung Lee <54669100+Closer94@users.noreply.github.com>
2022-05-01 16:01:06 +0900

    feature: jwt_회원가입 기능 구현

--- src/main/java/com/example/bsm/controller/UserController.java
+++ src/main/java/com/example/bsm/controller/UserController.java
@@ -20,14 +20,23 @@ public class UserController {
     private final JwtTokenProvider jwtTokenProvider;
     private final UserMapper userMapper;
 
+    // 회원가입
+    @PostMapping("/join")
+    public int join(@RequestBody Map<String, String> user) {
+        return userMapper.save(User.builder()
+                .email(user.get("email"))
+                .password(passwordEncoder.encode(user.get("password")))
+                .roles(Collections.singletonList("ROLE_ADMIN")) // 최초 가입시 USER 로 설정
+                .build());
+    }
 
     //로그인
     @PostMapping("/login")
-    public String login(@RequestBody Map<String, String> user){
+    public String login(@RequestBody Map<String, String> user) {
         User member = userMapper.findByEmail(user.get("email"))
                 .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 E-mail 입니다."));
 
-        if(!passwordEncoder.matches(user.get("password"), member.getPassword())){
+        if (!passwordEncoder.matches(user.get("password"),"{noop}" + member.getPassword())) {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
         return jwtTokenProvider.createToken(member.getUsername(), member.getRoles());
--- src/main/java/com/example/bsm/mapper/UserMapper.java
+++ src/main/java/com/example/bsm/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.example.bsm.mapper;
 
 import com.example.bsm.vo.User;
-import com.fasterxml.jackson.databind.deser.UnresolvedId;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +8,7 @@ import java.util.Optional;
 @Repository
 public interface UserMapper {
 
-    UnresolvedId save(User build);
+    int save(User build);
 
     Optional<User> findByEmail(String email);
 }
